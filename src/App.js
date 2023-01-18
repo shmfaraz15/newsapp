1b4e52164a078f55032538ef344a0c2263e965de
faraz <shm.faraz@15gmail.com>
2023-01-18 14:08:46 +0530

    Before Infinite scroll

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 5
   render() {
     return (
       <Router>
         <div>
           <NavBar />
           <Routes>
-            <Route path="/" element={<News key="general" pageSize={5} country="in" category="general" />} />
-            <Route path="/business" element={<News key="business" pageSize={5} country="in" category="business" />} />
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />} />
-            <Route path="/general" element={<News key="general" pageSize={5} country="in" category="general" />} />
-            <Route path="/health" element={<News key="health" pageSize={5} country="in" category="health" />} />
-            <Route path="/science" element={<News key="science" pageSize={5} country="in" category="science" />} />
-            <Route path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports" />} />
-            <Route path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology" />} />
+            <Route path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
+            <Route path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" />} />
+            <Route path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" />} />
+            <Route path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
+            <Route path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
           </Routes>
 
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
 
         return (
             <div className="card my-3" >
-                <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
                     {source}
                 </span>
                 <img src={imageUrl} className="card-img-top" alt="..." />
